f3823f9bde0ec5860a5e0f034f01469f5a82f8c1
tpinkhasov <32343740+tpinkhasov@users.noreply.github.com>
2025-01-23 15:25:01 -0600

    commit plot_ILIpdf changes commit colormaps folder with codes

--- Figures/plot_ILIpdf.m
+++ Figures/plot_ILIpdf.m
@@ -3,19 +3,28 @@ function plot_ILIpdf(TE, lickStruct, selectTrials, varargin)
 %% requires the use of getCorrectedInterLicks to generate the correct data structure
 
 defaults = {...
-    'plotBy', TE.animalID;...%do you want to plot the pdf for each animal vs trial type, etc.
+    'plotBy', 'animalID';...%do you want to plot the pdf for each animal vs trial type, etc.
     'lickVar', 'realInterLicks';,...%do you want to plot pdf for interlick intervals, time to first lick, etc.
     };
 [s, ~] = parse_args(defaults, varargin{:});
 
-loopBy = unique(s.plotBy(selectTrials));
+loopBy = unique(TE.(s.plotBy)(selectTrials));
 figure()
 for f = 1:length(loopBy)
     if ischar(loopBy{f}) == 1
-        trials = contains(s.plotBy, loopBy{f}) & selectTrials;
-        licks = cell2mat(lickStruct.(s.lickVar)(trials));
+        trials = contains(TE.(s.plotBy), loopBy{f}) & selectTrials;
+        if iscell(lickStruct.(s.lickVar))
+            licks = cell2mat(lickStruct.(s.lickVar)(trials)');
+        else
+            licks = lickStruct.(s.lickVar)(trials)';
+        end
         [d,xi] = ksdensity(licks, 'BandWidth', 0.12, 'Function','pdf');
         plot(xi,(d));
         hold on
     end
 end
+
+set(gca,'LineWidth',1,'TickDir','out', 'box', 'off', 'FontSize', 15);
+xlabel('Time (s)')
+ylabel('Probability density')
+legend(loopBy)
