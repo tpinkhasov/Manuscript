8c50a8c445cb585b183a67c8ffba6befff0f14c8
tpinkhasov <32343740+tpinkhasov@users.noreply.github.com>
2025-01-28 11:26:14 -0600

    edits

--- FigureGeneratingFunctions/plot_ILIpdf.m
+++ FigureGeneratingFunctions/plot_ILIpdf.m
@@ -1,6 +1,6 @@
-function plot_ILIpdf(TE, lickStruct, selectTrials, varargin)
-
 %% requires the use of getCorrectedInterLicks to generate the correct data structure
+
+function plot_ILIpdf(TE, lickStruct, selectTrials, varargin)
 
 defaults = {...
     'plotBy', 'animalID';...%do you want to plot the pdf for each animal vs trial type, etc.
@@ -13,17 +13,20 @@ loopBy = unique(TE.(s.plotBy)(selectTrials));
 colors = slanCM('cool',length(loopBy));
 figure()
 for f = 1:length(loopBy)
-    if ischar(loopBy{f}) == 1
+    if iscell(loopBy(f)) == 1 & ischar(loopBy{f}) == 1
         trials = contains(TE.(s.plotBy), loopBy{f}) & selectTrials;
-        if iscell(lickStruct.(s.lickVar))
-            licks = cell2mat(lickStruct.(s.lickVar)(trials)');
-        else
-            licks = lickStruct.(s.lickVar)(trials)';
-        end
-        [d,xi] = ksdensity(licks, 'BandWidth', s.bandWidth, 'Function','pdf');
-        plot(xi,(d), 'Color', [colors(f,:)], 'LineWidth', 2);;
-        hold on
+    else
+        trials = TE.(s.plotBy) == loopBy(f) & selectTrials;
     end
+    if iscell(lickStruct.(s.lickVar))
+        licks = cell2mat(lickStruct.(s.lickVar)(trials)');
+    else
+        licks = lickStruct.(s.lickVar)(trials)';
+    end
+    [d,xi] = ksdensity(licks, 'BandWidth', s.bandWidth, 'Function','pdf');
+    plot(xi,(d), 'Color', [colors(f,:)], 'LineWidth', 2);;
+    hold on
+end
 end
 
 set(gca,'LineWidth',1,'TickDir','out', 'box', 'off', 'FontSize', 15);
