5792af249f845d500a63cf66d237cd5e269e414b
tpinkhasov <32343740+tpinkhasov@users.noreply.github.com>
2025-01-28 08:52:11 -0600

    add getSessionIndex function

---
+++ TECreationFunctions/getSessionIndex.m
@@ -0,0 +1,17 @@
+%% getSessionIndex
+%% Indexes all sessions in a TE struct
+
+function sessionIndex = getSessionIndex(TE)
+
+uniqueSessions = unique(TE.filename, 'stable'); % get all the unique session names, keep the original order
+nSessions = length(uniqueSessions);
+nTrials = length(TE.filename);
+
+sessionIndex = NaN(nTrials,1); % initialize output array
+
+for s = 1:nSessions
+    trials = contains(TE.filename, uniqueSessions{s});
+    sessionIndex(trials) = s;
+end
+
+end
